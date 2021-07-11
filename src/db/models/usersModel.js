7a7125a56e95b72b87d49241e4a240b13c558ffa
Jakub Kret <jakubkret@Mac-mini-Jakub.local>
2021-07-11 11:25:58 +0200

    Add first model - user model

---
+++ src/db/models/usersModel.js
@@ -0,0 +1,43 @@
+const Sequelize = require('sequelize');
+const db = require('../config/database');
+
+const UsersModel = db.define('Users', {
+  userName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  userLastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  userCity: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  userAdressStreetName: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  userAdressStreetNumber: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  userAdressHomeNumber: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  userPhoneNumber: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  userUsername: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  userPassword: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = UsersModel;
