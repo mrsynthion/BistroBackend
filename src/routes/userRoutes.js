f384587219f72c6d1ceb06866c68a545cfb626a3
Jakub Kret <jakubkret@Mac-mini-Jakub.local>
2021-08-04 16:42:56 +0200

    Added jwt and bcrypt package and implement it in userRoutes file.

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,32 +2,42 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db/config/database');
 const Users = require('../db/models/account/usersModel');
+const bcrypt = require('bcrypt');
+const saltRounds = 2;
 
 router.get('/', (req, res) =>
   Users.findAll()
     .then((users) => {
+      res.statusCode = 200;
       res.json(users);
-      console.log(users);
-      res.sendStatus(200);
     })
     .catch((err) => console.log(err))
 );
 router.post('/addUser', (req, res) => {
-  Users.create({
-    userName: req.body.userName,
-    userLastName: req.body.userLastName,
-    userCity: req.body.userCity,
-    userAdressStreetName: req.body.userAdressStreetName,
-    userAdressStreetNumber: req.body.userAdressStreetNumber,
-    userAdressHomeNumber: req.body.userAdressHomeNumber,
-    userPhoneNumber: req.body.userPhoneNumber,
-    userUsername: req.body.userUsername,
-    userPassword: req.body.userPassword,
-  })
-    .then(() => res.sendStatus(201))
-    .catch((err) => {
-      res.status(500).send(err);
-    });
+  bcrypt
+    .hash(req.body.userPassword, saltRounds)
+    .then((hash) => {
+      Users.create({
+        userName: req.body.userName,
+        userLastName: req.body.userLastName,
+        userCity: req.body.userCity,
+        userAdressStreetName: req.body.userAdressStreetName,
+        userAdressStreetNumber: req.body.userAdressStreetNumber,
+        userAdressHomeNumber: req.body.userAdressHomeNumber,
+        userPhoneNumber: req.body.userPhoneNumber,
+        userUsername: req.body.userUsername,
+        userPassword: hash,
+      })
+        .then((result) => {
+          res.statusCode = 201;
+          res.send(result);
+        })
+        .catch((err) => {
+          res.statusCode = 500;
+          res.send(err);
+        });
+    })
+    .catch((err) => console.log(err));
 });
 
 router.post('/updateUser', (req, res) => {
@@ -46,10 +56,24 @@ router.post('/updateUser', (req, res) => {
     { where: { id: req.body.id } }
   )
     .then((result) => {
-      res.status(200).send(result);
+      res.statusCode = 200;
+      res.send(result);
     })
     .catch((err) => {
-      res.status(500).send(err);
+      res.statusCode = 500;
+      res.send(err);
+    });
+});
+
+router.delete('/deleteUser', (req, res) => {
+  Users.destroy({ where: { id: req.body.id } })
+    .then((res) => {
+      res.statusCode(200);
+      res.send(res);
+    })
+    .catch((err) => {
+      res.statusCode = 500;
+      res.send(err);
     });
 });
 
