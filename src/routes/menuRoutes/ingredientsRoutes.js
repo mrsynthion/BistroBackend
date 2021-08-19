5d9f5c16a01f5952f3e1d609c2e864107e9316c9
Kuba <>
2021-08-19 08:33:24 +0200

    Added get all route to ingredients routes

--- src/routes/menuRoutes/ingredientsRoutes.js
+++ src/routes/menuRoutes/ingredientsRoutes.js
@@ -2,14 +2,30 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../db/config/database');
 const Ingredients = require('../../db/models/menu/ingredientsModel');
+const { verifyAccess } = require('../../jwtTokens/verifyToken');
+const userTypes = require('../../../consts');
 
-router.get('/', (req, res) =>
-  Ingredients.findAll()
-    .then((ingredients) => {
-      console.log(ingredients);
-      res.sendStatus(200);
-    })
-    .catch((err) => console.log(err))
-);
+router.get('/', (req, res) => {
+  const data = verifyAccess(req, res);
+  if (!data) {
+    return;
+  }
+  if (
+    data &&
+    (data.userType === userTypes.USER ||
+      data.userType === userTypes.ADMIN ||
+      data.userType === userTypes.PERSONEL)
+  ) {
+    Ingredients.findAll()
+      .then((ingredients) => {
+        res.statusCode = 200;
+        res.json(ingredients);
+      })
+      .catch((err) => {
+        res.statusCode = 500;
+        res.json(err);
+      });
+  }
+});
 
 module.exports = router;
