e869caa6ee8ad54ed56cb1feaba2f6156bc24de9
Jakub Kret <jakubkret@Mac-mini-Jakub.local>
2021-08-19 20:49:34 +0200

    Added get and post routes for scheduleRoutes

--- src/routes/restaurantRoutes/scheduleRoutes.js
+++ src/routes/restaurantRoutes/scheduleRoutes.js
@@ -2,14 +2,60 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../db/config/database');
 const Schedule = require('../../db/models/restaurant/scheduleModel');
+const { verifyAccess } = require('../../jwtTokens/verifyToken');
+const userTypes = require('../../../consts');
 
-router.get('/', (req, res) =>
+router.get('/', (req, res) => {
   Schedule.findAll()
     .then((schedule) => {
-      console.log(schedule);
-      res.sendStatus(200);
+      res.statusCode = 200;
+      res.json(schedule);
     })
-    .catch((err) => console.log(err))
-);
+    .catch((err) => {
+      res.statusCode = 500;
+      res.json(err);
+    });
+});
+
+router.post('/addSchedule', (req, res) => {
+  const data = verifyAccess(req, res);
+  if (!data) {
+    return;
+  }
+  if (
+    data &&
+    (data.userType === userTypes.ADMIN || data.userType === userTypes.PERSONEL)
+  ) {
+    Schedule.findAll()
+      .then((schedule) => {
+        if (schedule.length < 8) {
+          Schedule.create({
+            scheduleWeekDay: req.body.scheduleWeekDay,
+            scheduleOpeningHour: req.body.scheduleOpeningHour,
+            scheduleCloseHour: req.body.scheduleCloseHour,
+          })
+            .then((scheduleDay) => {
+              res.statusCode = 201;
+              res.json(scheduleDay);
+            })
+            .catch((err) => {
+              res.statusCode = 500;
+              res.json({
+                message:
+                  'There is problem with adding schedule day to database',
+                err,
+              });
+            });
+        }
+      })
+      .catch((err) => {
+        res.statusCode = 500;
+        res.json({
+          message: 'There is problem with adding schedule day to database',
+          err,
+        });
+      });
+  }
+});
 
 module.exports = router;
