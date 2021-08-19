b5ae48e6c20e12a5f0bc91abce00ac68021213a7
Kuba <>
2021-08-19 12:52:55 +0200

    Ended up routes for menuItemsRoutes

--- src/routes/menuRoutes/menuItemsRoutes.js
+++ src/routes/menuRoutes/menuItemsRoutes.js
@@ -2,14 +2,107 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../db/config/database');
 const MenuItems = require('../../db/models/menu/menuItemsModel');
+const { verifyAccess } = require('../../jwtTokens/verifyToken');
+const userTypes = require('../../../consts');
 
-router.get('/', (req, res) =>
-  MenuItems.findAll()
-    .then((menuItems) => {
-      console.log(menuItems);
-      res.sendStatus(200);
+router.get('/', (req, res) => {
+  const data = verifyAccess(req, res);
+  if (!data) {
+    return;
+  }
+  if (
+    data &&
+    (data.userType === userTypes.USER ||
+      data.userType === userTypes.ADMIN ||
+      data.userType === userTypes.PERSONEL)
+  ) {
+    MenuItems.findAll()
+      .then((menuItems) => {
+        res.statusCode = 200;
+        res.json(menuItems);
+      })
+      .catch((err) => {
+        res.statusCode = 500;
+        res.json(err);
+      });
+  }
+});
+router.post('/addMenuItem', (req, res) => {
+  const data = verifyAccess(req, res);
+  if (!data) {
+    return;
+  }
+  if (
+    data &&
+    (data.userType === userTypes.ADMIN || data.userType === userTypes.PERSONEL)
+  ) {
+    MenuItems.create({
+      menuItemName: req.body.menuItemName,
+      menuItemCategory: req.body.menuItemCategory,
+      menuItemDescription: req.body.menuItemDescription,
+      menuItemPrice: req.body.menuItemPrice,
     })
-    .catch((err) => console.log(err))
-);
+      .then((menuItem) => {
+        res.statusCode = 201;
+        res.json(menuItem);
+      })
+      .catch((err) => {
+        res.statusCode = 500;
+        res.json(err);
+      });
+  }
+});
+
+router.post('/updateMenuItem', (req, res) => {
+  const data = verifyAccess(req, res);
+  if (!data) {
+    return;
+  }
+  if (
+    data &&
+    (data.userType === userTypes.ADMIN || data.userType === userTypes.PERSONEL)
+  ) {
+    MenuItems.update(
+      {
+        menuItemName: req.body.menuItemName,
+        menuItemCategory: req.body.menuItemCategory,
+        menuItemDescription: req.body.menuItemDescription,
+        menuItemPrice: req.body.menuItemPrice,
+      },
+      { where: { id: req.body.id } }
+    )
+      .then((menuItem) => {
+        res.statusCode = 200;
+        res.json(menuItem);
+      })
+      .catch((err) => {
+        res.statusCode = 500;
+        res.json(err);
+      });
+  }
+});
+
+router.delete('/deleteMenuItem', (req, res) => {
+  const data = verifyAccess(req, res);
+  if (!data) {
+    return;
+  }
+  if (
+    data &&
+    (data.userType === userTypes.ADMIN || data.userType === userTypes.PERSONEL)
+  ) {
+    MenuItems.destroy({ where: { id: req.body.id } })
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        res.statusCode = 500;
+        res.send(err);
+      });
+  } else {
+    res.statusCode = 401;
+    res.redirect('/');
+  }
+});
 
 module.exports = router;
