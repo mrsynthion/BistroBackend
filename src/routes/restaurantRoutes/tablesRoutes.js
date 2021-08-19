8c85ac16ed1727decc1d3b78cff6525d24d1f999
Jakub Kret <jakubkret@Mac-mini-Jakub.local>
2021-08-19 21:48:49 +0200

    Ended up routes for tablesRoutes

--- src/routes/restaurantRoutes/tablesRoutes.js
+++ src/routes/restaurantRoutes/tablesRoutes.js
@@ -2,14 +2,82 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../db/config/database');
 const Tables = require('../../db/models/restaurant/tablesModel');
+const { verifyAccess } = require('../../jwtTokens/verifyToken');
+const userTypes = require('../../../consts');
 
-router.get('/', (req, res) =>
+router.get('/', (req, res) => {
   Tables.findAll()
     .then((tables) => {
-      console.log(tables);
-      res.sendStatus(200);
+      res.statusCode = 200;
+      res.json(tables);
     })
-    .catch((err) => console.log(err))
-);
+    .catch((err) => {
+      res.statusCode = 500;
+      res.json(err);
+    });
+});
+
+router.post('/addTable', (req, res) => {
+  const data = verifyAccess(req, res);
+  if (!data) {
+    return;
+  }
+  if (
+    data &&
+    (data.userType === userTypes.ADMIN || data.userType === userTypes.PERSONEL)
+  ) {
+    Tables.findAll()
+      .then((tables) => {
+        if (tables.length < 8) {
+          Tables.create({
+            tableQuantityOfSeats: req.body.tableQuantityOfSeats,
+          })
+            .then((table) => {
+              res.statusCode = 201;
+              res.json(table);
+            })
+            .catch((err) => {
+              res.statusCode = 500;
+              res.json({
+                message: 'There is problem with adding table day to database',
+                err,
+              });
+            });
+        }
+      })
+      .catch((err) => {
+        res.statusCode = 500;
+        res.json({
+          message: 'There is problem with adding table day to database',
+          err,
+        });
+      });
+  }
+});
+router.post('/updateTable', (req, res) => {
+  const data = verifyAccess(req, res);
+  if (!data) {
+    return;
+  }
+  if (
+    data &&
+    (data.userType === userTypes.ADMIN || data.userType === userTypes.PERSONEL)
+  ) {
+    Tables.update({
+      tableQuantityOfSeats: req.body.tableQuantityOfSeats,
+    })
+      .then((table) => {
+        res.statusCode = 201;
+        res.json(table);
+      })
+      .catch((err) => {
+        res.statusCode = 500;
+        res.json({
+          message: 'There is problem with updating table day in database',
+          err,
+        });
+      });
+  }
+});
 
 module.exports = router;
